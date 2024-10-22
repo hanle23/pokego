ba7a3b70667b5ad78fd28f88f0e6afd70ce0d60f
Han Le <hanle.cs23@gmail.com>
2024-10-22 14:02:20 -0400

    Added Pagination Function for Berry firmness, Added Pagination Function for Berry Flavor, Added testings

--- berries.go
+++ berries.go
@@ -44,12 +44,42 @@ func (c *Client) BerryFirmness(id string) (result models.BerryFirmness, err erro
 	return result, err
 }
 
+func (c *Client) BerryFirmnesses(offset string, limit string) (result models.BerryFirmnesses, err error) {
+	_, err = strconv.Atoi(offset)
+	if err != nil {
+		return result, errors.New("Offset is not a valid number")
+	}
+	_, err = strconv.Atoi(limit)
+	if err != nil {
+		return result, errors.New("Limit is not a valid number")
+	}
+	targetURL := "berry-firmness/"
+	fullURL := fmt.Sprintf("%s?offset=%s&limit=%s", targetURL, offset, limit)
+	err = c.apiClient.Fetch(fullURL, &result)
+	return result, err
+}
+
 func (c *Client) BerryFlavor(id string) (result models.BerryFlavor, err error) {
 	if strings.TrimSpace(id) == "" {
 		return result, errors.New("Berry identifier cannot be empty")
 	}
 	targetURL := "berry-flavor"
 	fullURL := fmt.Sprintf("%s/%s/", targetURL, id)
+	err = c.apiClient.Fetch(fullURL, &result)
+	return result, err
+}
+
+func (c *Client) BerryFlavors(offset string, limit string) (result models.BerryFlavors, err error) {
+	_, err = strconv.Atoi(offset)
+	if err != nil {
+		return result, errors.New("Offset is not a valid number")
+	}
+	_, err = strconv.Atoi(limit)
+	if err != nil {
+		return result, errors.New("Limit is not a valid number")
+	}
+	targetURL := "berry-flavor/"
+	fullURL := fmt.Sprintf("%s?offset=%s&limit=%s", targetURL, offset, limit)
 	err = c.apiClient.Fetch(fullURL, &result)
 	return result, err
 }
